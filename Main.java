1c4d0a4316fdfb45347f04fdb8dd6cf752d37d3a
Francisco <90025914+FRANsrc@users.noreply.github.com>
2021-09-03 02:42:25 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import javax.sound.midi.Sequence;
+
+public class Main {
+
+    private static int[] sequence = new int[5];
+
+    public static void main(String[] args) {
+
+
+    }
+
+    p
+}
+
+
+/*
+maybe create two methods? one to continue calling "retry" if number is invalid.
+ */
